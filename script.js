d3580ef16591c5611975a844982b7baac6fb6c0f
Aditya Rana <iamadityarana01@gmail.com>
2025-12-26 14:19:50 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ let imagebtn=document.querySelector("#image")
 let image=document.querySelector("#image img")
 let imageinput=document.querySelector("#image input")
 
-const Api_Url="https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=AIzaSyBZwUpktmLMP16uNHLR4dZMrh-nZrY4-RU"
+const Api_Url="https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key=AIzaSyBZwUpktmLMP16uNHLR4dZMrh-nZrY4-RU"
 
 let user={
     message:null,
